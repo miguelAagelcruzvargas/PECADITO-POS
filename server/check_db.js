bf875c8e949374c1df2f787ac630a5d527637a93
Miguel Angel Cruz Vargas <miguel@example.com>
2026-04-08 15:44:11 -0600

    Organización de repositorio: Scripts de utilidad movidos a tests/ e ignorados en git

--- server/check_db.js
+++
@@ -1,9 +0,0 @@
-
-import db from './src/db.js';
-db.query("SHOW TABLES", (err, rows) => {
-    console.log("TABLES:", rows);
-    db.query("SELECT * FROM negocios", (err2, rows2) => {
-        console.log("NEGOCIOS:", rows2);
-        process.exit();
-    });
-});
--- server/check_negocios.js
+++
@@ -1,6 +0,0 @@
-
-import db from './src/db.js';
-db.query("SELECT id, nombre, nombre_comercial FROM negocios", (err, rows) => {
-    console.log(JSON.stringify(rows, null, 2));
-    process.exit();
-});
